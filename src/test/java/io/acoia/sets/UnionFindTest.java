d951f237ef229a410ba0547d40b2a14e1ac8b7b1
Royce Ausburn <esapersona@royce.id.au>
2017-01-28 18:21:46 +1100

    Adds some more junit tests for UnionFind

--- src/test/java/io/acoia/sets/UnionFindTest.java
+++ src/test/java/io/acoia/sets/UnionFindTest.java
@@ -2,6 +2,8 @@ package io.acoia.sets;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 import io.acoia.sets.UnionFind;
@@ -37,8 +39,11 @@ public class UnionFindTest {
   public void testUnionFindAllSame() {
     UnionFind<Integer> uf = new UnionFind<>();
 
+    int size = 0;
     for (int i = -100; i < 100; i++) {
+      assertEquals(size++, uf.size());
       uf.addMember(i);
+      assertEquals(size, uf.size());
     }
     
     for (int i = -99; i < 100; i++) {
@@ -52,4 +57,105 @@ public class UnionFindTest {
       assertTrue(uf.equivalent(0, i));
     }
   }
+  
+  @Test
+  public void testPathCompressionBranch() {
+    UnionFind<Integer> uf = new UnionFind<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+    uf.union(1, 2);
+    uf.union(3, 4);
+    uf.union(5, 6);
+    uf.union(2, 3);
+    uf.union(4, 5);
+    assertEquals((Integer) 1, uf.find(2));
+    assertEquals((Integer) 1, uf.find(5));
+    
+  }
+  
+  @Test
+  public void testPathMembersTest() {
+    UnionFind<Integer> uf = new UnionFind<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+    Arrays.equals(uf.members(1).toArray(new Integer[0]), new Integer[] { 1 } );
+    Arrays.equals(uf.members(2).toArray(new Integer[0]), new Integer[] { 2 } );
+    Arrays.equals(uf.members(3).toArray(new Integer[0]), new Integer[] { 3 } );
+    Arrays.equals(uf.members(4).toArray(new Integer[0]), new Integer[] { 4 } );
+    Arrays.equals(uf.members(5).toArray(new Integer[0]), new Integer[] { 5 } );
+    Arrays.equals(uf.members(6).toArray(new Integer[0]), new Integer[] { 6 } );
+    
+    assertEquals(6, uf.size());
+    
+    uf.union(1, 6);
+    Arrays.equals(uf.members(1).toArray(new Integer[0]), new Integer[] { 1, 6 } );
+    Arrays.equals(uf.members(2).toArray(new Integer[0]), new Integer[] { 2 } );
+    Arrays.equals(uf.members(3).toArray(new Integer[0]), new Integer[] { 3 } );
+    Arrays.equals(uf.members(4).toArray(new Integer[0]), new Integer[] { 4 } );
+    Arrays.equals(uf.members(5).toArray(new Integer[0]), new Integer[] { 5 } );
+    Arrays.equals(uf.members(6).toArray(new Integer[0]), new Integer[] { 1, 6 } );
+
+    uf.union(6, 6);
+    Arrays.equals(uf.members(1).toArray(new Integer[0]), new Integer[] { 1, 6 } );
+    Arrays.equals(uf.members(2).toArray(new Integer[0]), new Integer[] { 2 } );
+    Arrays.equals(uf.members(3).toArray(new Integer[0]), new Integer[] { 3 } );
+    Arrays.equals(uf.members(4).toArray(new Integer[0]), new Integer[] { 4 } );
+    Arrays.equals(uf.members(5).toArray(new Integer[0]), new Integer[] { 5 } );
+    Arrays.equals(uf.members(6).toArray(new Integer[0]), new Integer[] { 1, 6 } );
+
+    uf.union(6, 5);
+    Arrays.equals(uf.members(1).toArray(new Integer[0]), new Integer[] { 1, 6, 5 } );
+    Arrays.equals(uf.members(2).toArray(new Integer[0]), new Integer[] { 2 } );
+    Arrays.equals(uf.members(3).toArray(new Integer[0]), new Integer[] { 3 } );
+    Arrays.equals(uf.members(4).toArray(new Integer[0]), new Integer[] { 4 } );
+    Arrays.equals(uf.members(5).toArray(new Integer[0]), new Integer[] { 1, 6, 5 } );
+    Arrays.equals(uf.members(6).toArray(new Integer[0]), new Integer[] { 1, 6, 5 } );
+
+    uf.union(4, 6);
+    Arrays.equals(uf.members(1).toArray(new Integer[0]), new Integer[] { 1, 6, 5, 4 } );
+    Arrays.equals(uf.members(2).toArray(new Integer[0]), new Integer[] { 2 } );
+    Arrays.equals(uf.members(3).toArray(new Integer[0]), new Integer[] { 3 } );
+    Arrays.equals(uf.members(4).toArray(new Integer[0]), new Integer[] { 1, 6, 5, 4 } );
+    Arrays.equals(uf.members(5).toArray(new Integer[0]), new Integer[] { 1, 6, 5, 4 } );
+    Arrays.equals(uf.members(6).toArray(new Integer[0]), new Integer[] { 1, 6, 5, 4 } );
+
+    uf.union(3, 2);
+    Arrays.equals(uf.members(1).toArray(new Integer[0]), new Integer[] { 1, 6, 5, 4 } );
+    Arrays.equals(uf.members(2).toArray(new Integer[0]), new Integer[] { 3, 2 } );
+    Arrays.equals(uf.members(3).toArray(new Integer[0]), new Integer[] { 3, 2 } );
+    Arrays.equals(uf.members(4).toArray(new Integer[0]), new Integer[] { 1, 6, 5, 4 } );
+    Arrays.equals(uf.members(5).toArray(new Integer[0]), new Integer[] { 1, 6, 5, 4 } );
+    Arrays.equals(uf.members(6).toArray(new Integer[0]), new Integer[] { 1, 6, 5, 4 } );
+    
+    uf.union(3, 6);
+    Arrays.equals(uf.members(1).toArray(new Integer[0]), new Integer[] { 1, 6, 5, 4, 3, 2 } );
+    Arrays.equals(uf.members(2).toArray(new Integer[0]), new Integer[] { 1, 6, 5, 4, 3, 2 } );
+    Arrays.equals(uf.members(3).toArray(new Integer[0]), new Integer[] { 1, 6, 5, 4, 3, 2 } );
+    Arrays.equals(uf.members(4).toArray(new Integer[0]), new Integer[] { 1, 6, 5, 4, 3, 2 } );
+    Arrays.equals(uf.members(5).toArray(new Integer[0]), new Integer[] { 1, 6, 5, 4, 3, 2 } );
+    Arrays.equals(uf.members(6).toArray(new Integer[0]), new Integer[] { 1, 6, 5, 4, 3, 2 } );
+    
+    
+
+  }
+  
+  @Test
+  public void testUnionFindNonexistantElement() {
+    UnionFind<Integer> uf = new UnionFind<>();
+    uf.addMember(1);
+    try {
+      uf.equivalent(1, 2);
+      fail("Expected an IllegalArgumentException as 2 is not in the UnionFind");
+    }
+    catch (IllegalArgumentException e) {
+      // Expected
+    }
+    try {
+      uf.find(2);
+      fail("Expected an IllegalArgumentException as 2 is not in the UnionFind");
+    }
+    catch (IllegalArgumentException e) {
+      // Expected
+    }
+
+  }
+  
+
+
 }
